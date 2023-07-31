26931db9585fb5f95cb023a4ed19d773397519ff
kyeo-hub <god22@126.com>
2023-07-31 15:40:01 +0800

    fix static error

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from chat_room import main as chat_room
 from gomoku_game import main as gomoku_game
 
 app = FastAPI()
-# app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
